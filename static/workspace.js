77772762902ef656718a5998fe6f85098a8a40f1
qichunren <whyruby@gmail.com>
2019-09-06 16:37:58 +0800

    Redesign data in vue app.

--- static/workspace.js
+++ static/workspace.js
@@ -1,26 +1,142 @@
-var image = null;
-var app = new Vue({
+window.addEventListener("load", function(event) {
+  var vue_app = new Vue({
   el: '#app',
   data: {
-    current_image: '/static/images/fruits.jpg',
-    images: ['/static/images/1.jpg', '/static/images/2.jpg', '/static/images/3.jpg', '/static/images/4.jpg', '/static/images/5.jpg', '/static/images/6.jpg', '/static/images/7.jpg' ],
-    object_categories: [{id: 1, name: "Apple"}, {id: 2, name: "Grape"}, {id: 3, name: "Watermelon"}, {id: 4, name: "Blueberry "}, {id: 5, name: "Banana"}, {id: 6, name: "Peach"}, {id: 7, name: "Melon"}],
-    mark_elements: []
-  }, 
+    svg_draw: null,
+    svg_bg_image: null,
+    images: [
+              {thumb_url:'/static/images/fruits.jpg', url:'/static/images/fruits.jpg', marks: []},
+              {thumb_url:'/static/images/1.jpg', url:'/static/images/1.jpg', marks: []},
+              {thumb_url:'/static/images/2.jpg', url:'/static/images/2.jpg', marks: []},
+              {thumb_url:'/static/images/3.jpg', url:'/static/images/3.jpg', marks: []},
+              {thumb_url:'/static/images/4.jpg', url:'/static/images/4.jpg', marks: []},
+              {thumb_url:'/static/images/5.jpg', url:'/static/images/5.jpg', marks: []},
+              {thumb_url:'/static/images/6.jpg', url:'/static/images/6.jpg', marks: []},
+              {thumb_url:'/static/images/7.jpg', url:'/static/images/7.jpg', marks: []},
+              {thumb_url:'/static/images/8.jpg', url:'/static/images/8.jpg', marks: []},
+              {thumb_url:'/static/images/9.jpg', url:'/static/images/9.jpg', marks: []}
+            ],
+    mark_categories: [
+                        {id: 1, name: "Apple"}, 
+                        {id: 2, name: "Grape"}, 
+                        {id: 3, name: "Watermelon"}, 
+                        {id: 4, name: "Blueberry "}, 
+                        {id: 5, name: "Banana"}, 
+                        {id: 6, name: "Peach"}, 
+                        {id: 7, name: "Melon"}
+                      ],
+    current_image: null
+  },
+  mounted: function () {
+    var _this = this;
+    var drag_start = false;
+    var drag_draw_element = null;
+    var drag_start_point = {x:-1, y:-1};
+    var drag_end_point = {x:-1, y:-1};
+    var drag_new_rect = false;
+    var mouse_draged = false;
+    this.svg_draw = SVG().addTo('#drawing').size(900, 500);
+    this.svg_bg_image = this.svg_draw.image("/static/images/1.jpg");
+
+    this.svg_draw.mousedown(function(e) {
+      var point = {x: e.offsetX, y: e.offsetY};
+      console.log("mousedown: point", point);
+      drag_start = true;
+      mouse_draged = false;
+      drag_start_point = point;
+    });
+
+    this.svg_draw.mousemove(function(e) {
+      console.log("mousemove");
+      var current_point = {x: e.offsetX, y: e.offsetY};
+      
+      if(drag_start) {
+        var points_5 = [[drag_start_point.x,drag_start_point.y], [current_point.x,drag_start_point.y], [current_point.x,current_point.y], [drag_start_point.x, current_point.y], [drag_start_point.x,drag_start_point.y]];
+        if(drag_draw_element != null) {
+          drag_draw_element.remove();
+        }
+        drag_draw_element = _this.svg_draw.polyline(points_5).fill('none').stroke({ width: 2, color: '#000'});
+      } else {
+
+          /*
+          vue_app.mark_elements.forEach(item =>{
+              var rect_box = item.svg_element.get(0);
+          });
+          */
+
+      }
+    });
+
+    this.svg_draw.mouseup(function(e) {
+      var point = {x: e.offsetX, y: e.offsetY};
+      var element_id = _this.current_image.marks.length;
+      console.log("mouseup: point", point);
+
+      var drag_distance_x = point.x - drag_start_point.x;
+      var drag_distance_y = point.y - drag_start_point.y;
+      if(drag_distance_x < 0) {
+        drag_distance_x = -drag_distance_x;  
+      }
+      if(drag_distance_y < 0) {
+        drag_distance_y = -drag_distance_y;  
+      }
+      var drag_offset_min = 7;
+      if(drag_start && (drag_distance_x > drag_offset_min || drag_distance_y > drag_offset_min)) {
+        mouse_draged = true;  
+      } else {
+        mouse_draged = false;
+      }
+
+      if(!mouse_draged) {
+        drag_start = false;
+        if(drag_draw_element != null) {
+          drag_draw_element.remove();
+          drag_draw_element = null;
+        }
+        return;
+      }
+
+      drag_start = false;
+
+      var element_name = _this.mark_categories[0].name;
+
+      var box_group = _this.svg_draw.group();
+
+      drag_draw_element.mouseover(function(e){
+        console.log("mouse hover on element.");
+      });
+
+      box_group.add(drag_draw_element);
+
+      var text = _this.svg_draw.text(element_name).attr({x:drag_start_point.x, y:drag_start_point.y });
+      // text.font({anchor: 'middle', size: 30, family: 'Helvetica'});
+      box_group.add(text);
+
+
+      _this.current_image.marks.push({name: element_name, svg_element: box_group, selected: false});
+      drag_draw_element = null;
+    });
+
+
+    if(this.current_image == null) {
+      this.selectImage(this.images[0]);
+    }
+
+  },
   methods: {
-    toggleElement: function(selectedElement) {
-      var element_rect = selectedElement.svg_element.get(0);
-      if(selectedElement.selected) {
-        selectedElement.selected = false;
+    toggleMark: function(selectedMark) {
+      var element_rect = selectedMark.svg_element.get(0);
+      if(selectedMark.selected) {
+        selectedMark.selected = false;
         element_rect.stroke({ width: 2, color: '#000'});
       } else {
 
-        this.mark_elements.forEach(item =>{
-          if(item != selectedElement) {
+        this.current_image.marks.forEach(item =>{
+          if(item != selectedMark) {
             item.selected = false;
             item.svg_element.get(0).stroke({ width: 2, color: '#000'});
           } else {
-            selectedElement.selected = false;
+            selectedMark.selected = false;
             element_rect.stroke({ width: 2, color: '#FF9900'});
           }
         });
@@ -29,109 +145,37 @@ var app = new Vue({
       
     },
 
-    removeElement: function(element, index) {
-      element.svg_element.remove();
-      this.$delete(this.mark_elements, index); 
+    removeMark: function(mark, index) {
+      mark.svg_element.remove();
+      this.$delete(this.current_image.marks, index); 
     },
 
-    selectImage: function(image_src) {
-      console.log("Loading ", image_src);
-      image.load(image_src);
+    selectImage: function(image_object) {
+      console.log("Loading ", image_object.url);
+      if(this.current_image == null || this.current_image.url != image_object.url) {
 
-      this.mark_elements.forEach(item =>{
-        item.svg_element.remove();
-      });
+        // Remove old svg marks first.
+        if(this.current_image != null) {
+          this.current_image.marks.forEach(item =>{
+            item.svg_element.remove();
+          });
+        }
 
-      this.mark_elements = [];
-      this.current_image = image_src;
-    }
+        if(image_object.marks.length > 0) {
+
+          image_object.marks.forEach(item =>{
+            item.svg_element.addTo(this.svg_draw);
+          });
+        }
+        
+        this.current_image = image_object;
+        this.svg_bg_image.load(image_object.url);  
+      } else {
+        console.log("Same image, not load again.");
+      }
+    
+    } // / selectImage
   }
-}); // new Vue
+  }); // new Vue
 
-var drawa = SVG().addTo('#drawing').size(900, 500);
-image = drawa.image('/static/images/fruits.jpg');
-
-var drag_start = false;
-var drag_draw_element = null;
-var drag_start_point = {x:-1, y:-1};
-var drag_end_point = {x:-1, y:-1};
-var drag_new_rect = false;
-var mouse_draged = false;
-
-drawa.mousedown(function(e) {
-  var point = {x: e.offsetX, y: e.offsetY};
-  console.log("mousedown: point", point);
-  drag_start = true;
-  mouse_draged = false;
-  drag_start_point = point;
-});
-
-drawa.mousemove(function(e) {
-  console.log("mousemove");
-  var current_point = {x: e.offsetX, y: e.offsetY};
-  
-  if(drag_start) {
-    var points_5 = [[drag_start_point.x,drag_start_point.y], [current_point.x,drag_start_point.y], [current_point.x,current_point.y], [drag_start_point.x, current_point.y], [drag_start_point.x,drag_start_point.y]];
-    if(drag_draw_element != null) {
-      drag_draw_element.remove();
-    }
-    drag_draw_element = drawa.polyline(points_5).fill('none').stroke({ width: 2, color: '#000'});
-  } else {
-
-      app.mark_elements.forEach(item =>{
-          var rect_box = item.svg_element.get(0);
-
-      });
-
-  }
-});
-
-drawa.mouseup(function(e) {
-  var point = {x: e.offsetX, y: e.offsetY};
-  var element_id = app.mark_elements.length;
-  console.log("mouseup: point", point);
-
-  var drag_distance_x = point.x - drag_start_point.x;
-  var drag_distance_y = point.y - drag_start_point.y;
-  if(drag_distance_x < 0) {
-    drag_distance_x = -drag_distance_x;  
-  }
-  if(drag_distance_y < 0) {
-    drag_distance_y = -drag_distance_y;  
-  }
-  var drag_offset_min = 7;
-  if(drag_start && (drag_distance_x > drag_offset_min || drag_distance_y > drag_offset_min)) {
-    mouse_draged = true;  
-  } else {
-    mouse_draged = false;
-  }
-
-  if(!mouse_draged) {
-    drag_start = false;
-    if(drag_draw_element != null) {
-      drag_draw_element.remove();
-      drag_draw_element = null;
-    }
-    return;
-  }
-
-  drag_start = false;
-
-  var element_name = "Mark "+element_id;
-
-  var box_group = drawa.group();
-
-  drag_draw_element.mouseover(function(e){
-    console.log("mouse hover on element.");
-  });
-
-  box_group.add(drag_draw_element);
-
-  var text = drawa.text(element_name).attr({x:drag_start_point.x, y:drag_start_point.y });
-  // text.font({anchor: 'middle', size: 30, family: 'Helvetica'});
-  box_group.add(text);
-
-
-  app.mark_elements.push({name: element_name, svg_element: box_group, selected: false});
-  drag_draw_element = null;
 });
